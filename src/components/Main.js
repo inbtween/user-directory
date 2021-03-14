8109c04f490331e41842602557a7ca249c405c25
Michelle Smith <smith.michelle.a@gmail.com>
2021-03-14 19:22:52 -0400

    Create a React app and publish it to GitHub Pages

---
+++ src/components/Main.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react"
+import SearchBar from "./SearchBar"
+import Table from "./Table"
+import Api from "../utils/Api"
+// need a Nav bar
+// api
+// table
+
+export default class Main extends Component {
+    state = {
+        users: [{}],
+        order: "descend",
+        filteredUsers: [{}]
+    }
+    handleSearchChange = event => {
+        console.log(event.target.value)
+        const filter = event.target.value
+        const filteredList = this.state.users.filter( item => {
+          let values = Object.values(item).join("").toLowerCase()
+          return values.indexOf(filter.toLowerCase()) !== -1
+        })
+        this.setState({
+            filteredUsers: filteredList
+        })
+    }
+    componentDidMount() {
+        Api.getUsers().then(results => {
+        
+            this.setState({
+                users: results.data.results,
+                filteredUsers: results.data.results
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+                <div>
+                    <Table/>
+                </div>
+            </div>
+        )
+    }
+
+}
